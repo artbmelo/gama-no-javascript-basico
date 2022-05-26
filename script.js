ee25f8f00c064c49bb84cd46963677dcf24eacea
Arthur Melo <arthur.mbmelo@gmail.com>
2022-05-26 20:02:04 -0300

    finalizando o código da validação de CPF

--- script.js
+++ script.js
@@ -8,24 +8,43 @@ function validaCPF(cpf) {
         var numeros = cpf.substring(0, 9);
         var digitos = cpf.substring(9);
 
+        console.log(numeros);
+        console.log(digitos);
+
         // VALIDAÇÃO DO PRIMEIRO DÍGITO
-        // Multiplica-se of 9 primeiros dígitos pela sequência decrescente de números de 10 à 2 e soma os resultados.
+        // Multiplica-se os 9 primeiros dígitos pela sequência decrescente de números de 10 à 2 e soma os resultados.
         var soma = 0;
         for (var i = 10; i > 1; i--) {
             soma += numeros.charAt(10 - i) * i;
         }
-        console.log(soma);
 
         var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+
+        console.log(resultado);
 
         // Validação do primeiro dígito.
         if (resultado != digitos.charAt(0)) {
             return false;
         }
 
-        console.log(
-            digitos.toString().charAt(0) + " é a primeira posição da variável soma"
-        );
+        // Zerando o valor da variável 'soma'.
+        soma = 0;
+
+        // Estabelecendo um novo escopo: 10 primeiros dígitos.
+        // Atribui esses dígitos à variável 'numeros'.
+        numeros = cpf.substring(0, 10);
+
+        // Multiplica-se os 10 primeiros dígitos pela sequência decrescente de números de 10 à 2 e soma os resultados.
+        for (var k = 11; k > 1; k--) {
+            soma += numeros.charAt(11 - k) * k;
+        }
+
+        resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        // Validação do segundo dígito.
+        if(resultado != digitos.charAt(1)){
+            return false;
+        }
 
         return true;
     }
@@ -33,6 +52,8 @@ function validaCPF(cpf) {
 
 function validacao() {
     console.log('Iniciando validação CPF.');
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
 
     var cpf = document.getElementById('cpf_digitado').value;
 
